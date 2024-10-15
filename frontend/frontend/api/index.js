7d1d9cf94a6af0398738a99ff498af1793d69225
JMC404 <96236737+JMC404@users.noreply.github.com>
2024-10-15 15:11:33 +0100

    various functions created for establishing connection with webSocket

---
+++ frontend/frontend/api/index.js
@@ -0,0 +1,26 @@
+
+
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+
+
+let connect = () => {
+    console.log("Attempting Connection...");
+
+
+    socket.onopen = () => {
+        console.log("Successfully Connected");
+    };
+
+    socket.onmessage = msg => {
+        console.log(msg);
+    };
+
+    socket.onclose = Event => {
+        console.log("Socket Closed Connection: ", Event);
+    };
+
+    socket.onerror = error => {
+        console.log("Socket Error", error)
+    };
+};
